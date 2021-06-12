3318e4c307571f77f1213f944d75202999896c38
Artur Jaworski <jawostreet97@gmail.com>
2021-06-12 15:02:07 +0200

    before add shuffle

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
-import mapreduce, filehandler
+import filehandler
+from mapreduce import MapReduce
 
 
-class CountCharacters(mapreduce.MapReduce):
+class CountCharacters(MapReduce):
 
     def mapper(self, key, value):
         results = []
@@ -13,7 +14,7 @@ class CountCharacters(mapreduce.MapReduce):
 
 
 if __name__ == "__main__":
-    num_mapper = 5
+    num_mapper = 4
     num_reducer = 2
     map_reduce = CountCharacters('shakespeare.txt', num_mapper, num_reducer)
     map_reduce.run()
